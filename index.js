a676fee96840ed56ab7de609e18ddaaa0ee1c7fe
Aditya Agarwal <adi790u@gmail.com>
2023-06-24 18:40:24 +0530

    reverted to some previous settings

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ app.get("/api/:date", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
-  console.log('Your app is listening on port ' + 3000);
+var listener = app.listen(process.env.port, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
 
 // listener.address().port
